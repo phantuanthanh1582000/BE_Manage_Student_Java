c2afa7cdcf956b5a138359389aa43757163189e5
DESKTOP-IJI51F8\ADMIN <tphan10932@gmail.com>
2025-05-23 08:45:28 +0700

    no message

--- src/main/java/com/example/studentmanagement/repositories/RoomRepository.java
+++ src/main/java/com/example/studentmanagement/repositories/RoomRepository.java
@@ -2,13 +2,14 @@ package com.example.studentmanagement.repositories;
 
 import java.util.List;
 
-
-import com.example.studentmanagement.models.RoomModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.studentmanagement.models.RoomModel;
+
 @Repository
 public interface RoomRepository extends MongoRepository<RoomModel, String> {
+    @Override
     List<RoomModel> findAll();
-
+    boolean existsByNameIgnoreCase(String name);
 }
